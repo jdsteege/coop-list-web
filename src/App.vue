60ef0b0561400c3243ee72988078c16cd3b8d6d2
jdsteege <jdsteege@gmail.com>
2023-02-24 17:29:48 -0600

    Fixing Firebase hosting workflow

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@ import TheWelcome from "./components/TheWelcome.vue";
     />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it! 3" />
+      <HelloWorld msg="You did it! Welcome to Coop Lists" />
     </div>
   </header>
 
